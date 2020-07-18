ad9002012bc09682df819c7bdeaa71c3bbbc753f
haidousm <haidous.m@gmail.com>
2020-07-18 15:38:42 +0300

    draw ball and paddle

--- Project-Four/script.js
+++ Project-Four/script.js
@@ -2,6 +2,58 @@ const rulesBtn = document.getElementById("rules-btn");
 const closeBtn = document.getElementById("close-btn");
 const rules = document.getElementById("rules");
 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+let score = 0;
+
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+};
+
+const paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
+};
+
+const drawBall = () => {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, 2 * Math.PI);
+    ctx.fillStyle = "#0095dd";
+    ctx.fill();
+    ctx.closePath();
+};
+
+const drawPaddle = () => {
+    ctx.beginPath();
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+    ctx.fillStyle = "#0095dd";
+    ctx.fill();
+    ctx.closePath();
+};
+
+const drawScore = () => {
+    ctx.font = "20px Arial";
+    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+};
+
+const draw = () => {
+    drawBall();
+    drawPaddle();
+    drawScore();
+};
+
+draw();
+
 rulesBtn.addEventListener("click", () => {
     rules.classList.add("show");
 });
